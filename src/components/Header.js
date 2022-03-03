67c60f8e4988ddd4ba28fed1534bebed274997c9
Elcid-Chawa <elcidchawa@gmail.com>
2022-03-03 05:39:20 +0100

    adjusted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import achats3 from '../assets/img/achats3.jpg'
 
 export default function Header(){
     return(
-        <header className="bd-header bg-image py-5"
-        style={{ 
-            backgroundImage: `url(${achats3})`, 
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-        }}>
-            <Container 
-                style={{
-                    display: "flex",
+        <Row className="align-items-center">
+            <Container className="d-flex bd-header bg-image justify-content-center aling-items-center"
+            style={{ 
+                backgroundImage: `url(${achats3})`, 
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundAttachment: "fixed",
+                height: "500px"
+            }}>                
                     
-                }} >
-                <Row style={{paddingLeft: '50%'}}>
-                    <h1 className="text-black" >SERVICE TECHNIQUE</h1>
-                </Row>
-                 
             </Container>
-        </header>
+            <Container className="justify-content-center aling-items-center"><h1 className="text-black py-" >SERVICE TECHNIQUE</h1></Container>
+        </Row>
+        
         
     )
 }
