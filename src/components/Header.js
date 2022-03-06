790760461bc7482dc880c7fd81d18bb88d7a3e7e
Elcid-Chawa <elcidchawa@gmail.com>
2022-03-06 07:22:54 +0100

    added service

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ export default function Header(){
             <Col className="justify-content-left aling-items-start text-white"
                 style={{paddingLeft: "50px"}}
             >                
-                <h1 style={{fontSize: "60px", color: "orange"}} >SERVICE TECHNIQUE</h1>
+                <h1 style={{fontSize: "60px", color: "orange"}} >SERVICE TECHNIQUE BIR</h1>
                 <p style={{fontSize: "20px",width:"50%"}}>Un service technique qui gère les installations, les maintenance, les achats, les devis, le personnel, l administration, les missions, les interventions sur tous les appareils (électriques, chambre froide , clim, foyers, frigos, fours)  tout ce qui concerne l’industrie .</p>
                 <Button style={{backgroundColor: "orange"}}>Nos service</Button>
             </Col>
